2ab4090f53710790f849d276d80cb6d9040e3211
zzpicanfiy <993420019@qq.com>
2019-01-17 19:15:59 +0800

    用户实体类

---
+++ src/User.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class User {
+    public String username;
+    public int age ;
+
+    public User() {
+    }
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
